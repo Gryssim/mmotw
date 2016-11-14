ea716527deb62717e407f392599c4df5d43d4c1f
Doug <dgplock@gmail.com>
2016-11-14 14:35:04 -0700

    Fixed nesting issue of today's events with scheduled events. Fixed nesting of subsequent events on events with the same date. Removed debugging borders.

--- index.php
+++ index.php
@@ -21,10 +21,6 @@ $events = $taskModel->getTaskBlobByDate($selectedDate, $_SESSION["user"]["user_n
 require_once("./include/header.php");
 // <body> tag opened in required header.php
 ?>
-
-<?php
-echo "&nbsp;&nbsp;&nbsp;&nbsp;$selectedDate";
-?>
     <div class="container-fluid">
         <form method="POST" target="_self">
             <label> Select a Date: &nbsp; </label><input type="date" name="date" placeholder="YY-MM-DD"/>
@@ -32,62 +28,63 @@ echo "&nbsp;&nbsp;&nbsp;&nbsp;$selectedDate";
         </form>
         <br/>
         <br/>
-        <div class="row">
-            <div class="col-md-3" style="border-style: solid;">
-                <?php
-                //TODO: Re-order week based on $selectedDate as first day, and advance 7 days.
-                $scheduleDate = $selectedDate;
-                $i =  date("w", strtotime($selectedDate));
-                for($j = 0; $j < 7; $j++){
-                    $daysTasks = $taskModel->getTaskBlobByDate($scheduleDate, $_SESSION["user"]["user_name"]);
-                    //var_dump($daysTasks);
-                    echo "<div>";
-                    echo "<label class='dayTitle'>$dayOfTheWeek[$i]&nbsp;&nbsp;" . date("D, M d", strtotime($scheduleDate)) . "</label>";
-                    if(sizeof($daysTasks) > 0){
-                        foreach($daysTasks as $tasks) {
-                            echo "<ul><li class='taskTitle'>" . ucfirst($tasks["task_title"]) . "</li></ul>";
-                        }
-                    } else {
-                        echo "<ul><li>There are no tasks scheduled for this day.</li></ul>";
-                    }
 
-                    $i++;
-                    if($i >= 7){
-                        $i = 0;
+        <div class="col-md-3">
+            <?php
+            //TODO: Re-order week based on $selectedDate as first day, and advance 7 days.
+            $scheduleDate = $selectedDate;
+            $i =  date("w", strtotime($selectedDate));
+            for($j = 0; $j < 7; $j++){
+                $daysTasks = $taskModel->getTaskBlobByDate($scheduleDate, $_SESSION["user"]["user_name"]);
+                //var_dump($daysTasks);
+                echo "<div>";
+                echo "<label class='dayTitle'>$dayOfTheWeek[$i]&nbsp;&nbsp;" . date("D, M d", strtotime($scheduleDate)) . "</label><ul>";
+                if(sizeof($daysTasks) > 0){
+                    foreach($daysTasks as $tasks) {
+                        echo "<li class='scheduleTask'>" . ucfirst($tasks["task_title"]) . "</li>";
                     }
-
-                    $scheduleDate = date("y-m-d", strtotime("$scheduleDate +1 day"));
-                }
-                /*foreach($dayOfTheWeek as $day) {
-                    echo "<div>";
-                    echo "<label>$day</label>";
-                    echo "<ul>";
-                    //TODO: serve each days events without description.
-                    echo "<li>Do something</li>";
-                    echo "</div><br/>";
-                } */
-                ?>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-6 todayEvents" style="border-style: solid;">
-                <?php
-                // var_dump for debug purposes.
-                //var_dump($events);
-                if(sizeof($events) > 0){
-                    //TODO: List events along with default events.
-                    echo "<ul>";
-                    foreach($events as $event){
-                        echo "<li class=\"taskTitle\">" . ucfirst($event["task_title"]) . "</li>";
-                        echo "<ul> <li class='taskDescription''> " . $event["task_description"] . "</li></ul>";
-                    }
+                    echo "</ul></div>";
                 } else {
-                    echo "<h3> There are no events set for today.
-                              (" . date("D, M d", strtotime($selectedDate)) . ")</h3>";
+                    echo "<li class='scheduleTask'>There are no tasks scheduled for this day.</li></ul></div>";
                 }
-                ?>
-            </div>
+
+                $i++;
+                if($i >= 7){
+                    $i = 0;
+                }
+
+                $scheduleDate = date("y-m-d", strtotime("$scheduleDate +1 day"));
+            }
+            /*foreach($dayOfTheWeek as $day) {
+                echo "<div>";
+                echo "<label>$day</label>";
+                echo "<ul>";
+                //TODO: serve each days events without description.
+                echo "<li>Do something</li>";
+                echo "</div><br/>";
+            } */
+            ?>
         </div>
+
+
+        <div class="col-lg-6 todayEvents">
+            <?php
+            // var_dump for debug purposes.
+            //var_dump($events);
+            if(sizeof($events) > 0){
+                //TODO: List events along with default events.
+                echo "<label class='dayTitle'> Events for: " . date("D, M d", strtotime($selectedDate)) . "</label>";
+                foreach($events as $event){
+                    echo "<li class=\"taskTitle\">" . ucfirst($event["task_title"]) . "</li>";
+                    echo "<li class='taskDescription''> " . $event["task_description"] . "</li>";
+                }
+            } else {
+                echo "<h3> There are no events set for today.
+                              (" . date("D, M d", strtotime($selectedDate)) . ")</h3>";
+            }
+            ?>
+        </div>
+
     </div>
 
 </body>
